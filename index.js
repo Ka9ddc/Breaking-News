4d6cbe5741f0762e93d6b517ef1eb36bb10c8bae
Ryan Micael <ka9ddcgamer@gmail.com>
2023-01-15 14:21:54 -0300

    method added: post method tested without real data

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const userRoute = require("./src/routes/user.route");
 
-app.use("/soma", userRoute);
+app.use(express.json());
+app.use("/user", userRoute);
 
 const port = 8080;
 app.listen(port, () => {
