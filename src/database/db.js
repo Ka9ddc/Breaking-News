4538b0640d0cebf0ef04eee5579b8551324d0233
Ryan Micael <ka9ddcgamer@gmail.com>
2023-01-16 11:36:15 -0300

    refactor: create user function builded successful

---
+++ src/database/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+  console.log("Wait conecting to database!");
+  mongoose.set("strictQuery", false);
+  mongoose
+    .connect(
+      "mongodb+srv://admin:ka9ddc123@cluster0.gwvrxna.mongodb.net/?retryWrites=true&w=majority",
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => {
+      console.log("MongoDB Atlas Connected");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+module.exports = connectDatabase;
