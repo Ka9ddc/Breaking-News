8cf6ef1fff8c1f41fdc2455a903030a341424dc1
Ryan Micael <ka9ddcgamer@gmail.com>
2023-01-27 00:15:25 -0300

    feat: middleware for validation of JWT created

--- src/routes/News.route.js
+++ src/routes/News.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { create, findAll } from "../controllers/news.controller.js";
 const router = Router();
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
-router.post("/", create);
+router.post("/", authMiddleware, create);
 router.get("/", findAll);
 
 export default router;
