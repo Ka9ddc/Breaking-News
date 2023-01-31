3664a41010cda75ce7af840485fae8ead58023a8
Ryan Micael <ka9ddcgamer@gmail.com>
2023-01-31 13:38:16 -0300

    feat: created documentation to API with Swagger

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
 import newsRoute from "./src/routes/news.route.js";
+import swaggerRoute from "./src/routes/swagger.route.js";
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/news", newsRoute);
+app.use("/doc", swaggerRoute);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
